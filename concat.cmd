6166024c76ae6198296f4353e7227de344b6d974
전지운 <jeonju@easyendosurgical.com>
2019-10-01 10:23:48 +0900

    concat output 위치 수정

--- concat.cmd
+++ concat.cmd
@@ -1,11 +1,18 @@
 set preset=slow
 set filter=*.mp4
-set outdir=out
+set outdir=v
 set ratecontrol=vbr_hq
 set avgbitrate=6M
+set concatfile=concat.txt
 
-mkdir %outdir%
+del %concatfile%
+FOR %%f IN (%filter%) DO echo file '%%f' >> %concatfile%
 
-ffmpeg -f concat -safe 0 -i concat.txt -c copy %outdir%/concat_output.mp4
+
+
+mkdir "%outdir%"
+
+
+ffmpeg -f concat -safe 0 -i %concatfile% -c copy "%outdir%/concat_output.mp4"
 
 @pause
